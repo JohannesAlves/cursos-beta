dad486c88dceea7552afe8b6359d707037ece2b7
johannesalves <johannes001002@gmail.com>
2024-03-19 22:30:43 -0300

    refactor: chore extends button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,11 @@
 import { IButtonProps } from './types';
 
-export const Button: React.FC<IButtonProps> = ({ text, size }) => {
+export const Button: React.FC<IButtonProps> = ({ text, ...props }) => {
   return (
-    <button className="text-gray-400 border-2  border-gray-700 rounded-lg hover:text-red-500 transition-all w-52 h-10">
+    <button
+      className="text-gray-400 border-2  border-gray-700 rounded-lg hover:text-red-500 transition-all w-52 h-10"
+      {...props}
+    >
       {text}
     </button>
   );
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,4 @@
-export interface IButtonProps {
+export interface IButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
   size?: 'sm' | 'md' | 'lg';
 }
