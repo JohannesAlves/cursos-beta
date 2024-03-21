34a33ebab4ef8b4eba16a7670a3fa3991dfbbf06
johannesalves <johannes001002@gmail.com>
2024-03-20 23:26:36 -0300

    deploy: next config the cors

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const nextConfig = {
       source: '/api/:path*',
       headers: [
         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+        {
+          key: 'Access-Control-Allow-Origin',
+          value:
+            'https://cursos-beta-4pb36vkor-johannes-projects-6f05f567.vercel.app',
+        }, // replace this your actual origin
         {
           key: 'Access-Control-Allow-Methods',
           value: 'GET,DELETE,PATCH,POST,PUT',
